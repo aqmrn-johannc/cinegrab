524800bd9635423f3a95ea0584372151457e0c3f
Johann C <joso.custodio.up@phinmaed.com>
2024-09-28 00:17:18 +0800

    initial commit

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans text-gray-900 antialiased" style="background-image: url('{{ asset('images/bgimage.png') }}'); background-size: cover; background-position: center;">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
+            <div>
+                <a href="/">
+                    <img src="{{ asset('images/cinegrablogo.png') }}" style="width:400px; height: auto;">
+                </a>
+                <p style="color: white; font-size: 1.25rem; text-align: center; margin-top: 10px;">
+                    Make your movie ticket booking easier!
+                </p>
+            </div>
+
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
+        </div>
+    </body>
+</html>
